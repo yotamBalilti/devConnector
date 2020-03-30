d41606e13843a7abb515f83527ba874a38f0b21a
Yotam <yotam.balilti@gmail.com>
2020-03-30 15:13:26 +0300

    added logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Landing } from './components/layout/Landing';
-import { Navbar } from './components/layout/Navbar';
+import Navbar from './components/layout/Navbar';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
